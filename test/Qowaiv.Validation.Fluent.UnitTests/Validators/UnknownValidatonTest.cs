3474aca3f3f2f29556ac51cb6e169fd2a86262cf
Corniel Nobel <mail@corniel.nl>
2020-03-25 13:20:00 +0100

    Rewrote all existing rules to not use custom validators, but only the Fluent API.

---
+++ test/Qowaiv.Validation.Fluent.UnitTests/Validators/UnknownValidatonTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using Qowaiv.Globalization;
+using Qowaiv.Validation.Fluent.UnitTests.Models;
+using Qowaiv.Validation.TestTools;
+using System.Globalization;
+
+namespace Qowaiv.Validation.Fluent.UnitTests.Validators
+{
+    public class UnknownValidatonTest
+    {
+        [Test]
+        public void Known_IsValid()
+        {
+            var model = new NotUnknownModel { Email = EmailAddress.Parse("test@qowaiv.org") };
+            FluentValidatorAssert.IsValid<NotUnknownModelValidator, NotUnknownModel>(model);
+        }
+
+        [TestCase("'Email' must not be empty.", "en-GB")]
+        [TestCase("'Email' mag niet leeg zijn.", "nl-BE")]
+        public void Empty(string message, CultureInfo culture)
+        {
+            using (new CultureInfoScope(culture))
+            {
+                var model = new NotUnknownModel { Email = EmailAddress.Empty };
+
+                FluentValidatorAssert.WithErrors<NotUnknownModelValidator, NotUnknownModel>(model,
+                    ValidationMessage.Error(message, "Email")
+                );
+            }
+        }
+
+        [TestCase("'Email' must not be unknown.", "en-GB")]
+        [TestCase("'Email' mag niet onbekend zijn.", "nl-BE")]
+        public void Unknown(string message, CultureInfo culture)
+        {
+            using (new CultureInfoScope(culture))
+            {
+                var model = new NotUnknownModel { Email = EmailAddress.Unknown };
+
+                FluentValidatorAssert.WithErrors<NotUnknownModelValidator, NotUnknownModel>(model,
+                    ValidationMessage.Error(message, "Email")
+                );
+            }
+        }
+    }
+}
