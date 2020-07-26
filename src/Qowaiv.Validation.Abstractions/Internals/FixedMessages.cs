faf7facfd3a5e2e29a348693a62947b20899b04c
Corniel Nobel <mail@corniel.nl>
2020-07-26 18:00:54 +0200

    Prevent copying of message collections by using an (internal) inmutable/fixed message collection.

---
+++ src/Qowaiv.Validation.Abstractions/Internals/FixedMessages.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Qowaiv.Validation.Abstractions.Internals
+{
+    [DebuggerDisplay("{DebuggerDisplay}"), DebuggerTypeProxy(typeof(CollectionDebugView))]
+    internal class FixedMessages : IReadOnlyList<IValidationMessage>
+    {
+        public static FixedMessages New(IEnumerable<IValidationMessage> messages)
+            => messages is FixedMessages @internal
+                ? @internal
+                : Empty.AddRange(messages);
+
+        public static readonly FixedMessages Empty = new FixedMessages();
+
+        public virtual int Count => 0;
+
+        public FixedMessages Add(IValidationMessage message)
+            => message.Severity > ValidationSeverity.None
+            ? new SomeFixedMessages(this, message)
+            : this;
+
+        public FixedMessages AddRange(IEnumerable<IValidationMessage> elements)
+        {
+            var next = this;
+            foreach(var element in elements)
+            {
+                next = next.Add(element);
+            }
+            return next;
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private string DebuggerDisplay => $"Count = {Count}";
+
+        public IValidationMessage this[int index] => this.Skip(index).FirstOrDefault() ?? throw new IndexOutOfRangeException();
+
+        public virtual IEnumerator<IValidationMessage> GetEnumerator() => Enumerable.Empty<IValidationMessage>().GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+}
---
+++ src/Qowaiv.Validation.Abstractions/Internals/SomeFixedMessages.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Qowaiv.Validation.Abstractions.Internals
+{
+    internal class SomeFixedMessages : FixedMessages
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly FixedMessages parent;
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly IValidationMessage value;
+
+        public SomeFixedMessages(FixedMessages parent, IValidationMessage value)
+        {
+            this.parent = parent;
+            this.value = value;
+        }
+        
+        public override int Count => parent.Count+1;
+
+        public override IEnumerator<IValidationMessage> GetEnumerator() => Enumerate().Reverse().GetEnumerator();
+
+        private IEnumerable<IValidationMessage> Enumerate()
+        {
+            FixedMessages current = this;
+            while (current is SomeFixedMessages nonEmpty)
+            {
+                yield return nonEmpty.value;
+                current = nonEmpty.parent;
+            }
+        }
+    }
+}
