7f3893bfce5dbf1d5927f00b6fef6dbf30ff4f4e
Corniel Nobel <mail@corniel.nl>
2021-02-02 10:40:54 +0100

    Introduction of common messages (#22)

---
+++ src/Qowaiv.Validation.Messages/AccessDenied.cs
@@ -0,0 +1,51 @@
+﻿using Qowaiv.Validation.Abstractions;
+using System;
+using System.Runtime.Serialization;
+using System.Security;
+
+namespace Qowaiv.Validation.Messages
+{
+    /// <summary>Message to communicate that access has been denied.</summary>
+    [Serializable]
+    public class AccessDenied : SecurityException, IValidationMessage
+    {
+        /// <summary>Initializes a new instance of the <see cref="AccessDenied"/> class.</summary>
+        public AccessDenied() : this(ValidationMessages.AccessDenied) => Do.Nothing();
+
+        /// <summary>Initializes a new instance of the <see cref="AccessDenied"/> class.</summary>
+        public AccessDenied(string message) : base(message) => Do.Nothing();
+
+        /// <summary>Initializes a new instance of the <see cref="AccessDenied"/> class.</summary>
+        public AccessDenied(string message, Exception inner) : base(message, inner) => Do.Nothing();
+
+        /// <summary>Initializes a new instance of the <see cref="AccessDenied"/> class.</summary>
+        /// <param name="message">
+        /// The error message that explains the reason for the exception.
+        /// </param>
+        /// <param name="type">
+        /// The type of the permission that caused the exception to be thrown.
+        /// </param>
+        public AccessDenied(string message, Type type) : base(message, type) => Do.Nothing();
+
+        /// <summary>Initializes a new instance of the <see cref="AccessDenied"/> class.</summary>
+        /// <param name="message">
+        /// The error message that explains the reason for the exception.
+        /// </param>
+        /// <param name="type">
+        /// The type of the permission that caused the exception to be thrown.
+        /// </param>
+        /// <param name="state">
+        /// The state of the permission that caused the exception to be thrown.
+        /// </param>
+        public AccessDenied(string message, Type type, string state) : base(message, type, state) => Do.Nothing();
+
+        /// <summary>Initializes a new instance of the <see cref="AccessDenied"/> class.</summary>
+        protected AccessDenied(SerializationInfo info, StreamingContext context) : base(info, context) => Do.Nothing();
+
+        /// <inheritdoc />
+        public ValidationSeverity Severity => ValidationSeverity.Error;
+
+        /// <inheritdoc />
+        public string PropertyName => null;
+    }
+}
---
+++ src/Qowaiv.Validation.Messages/EntityNotFound.cs
@@ -0,0 +1,33 @@
+﻿using Qowaiv.Validation.Abstractions;
+using System;
+using System.Runtime.Serialization;
+
+namespace Qowaiv.Validation.Messages
+{
+    /// <summary>Message to communicate if a entity could not be found.</summary>
+    [Serializable]
+    public class EntityNotFound : InvalidOperationException, IValidationMessage
+    {
+        /// <summary>Initializes a new instance of the <see cref="EntityNotFound"/> class.</summary>
+        public EntityNotFound() : this(ValidationMessages.EntityNotFound) => Do.Nothing();
+
+        /// <summary>Initializes a new instance of the <see cref="EntityNotFound"/> class.</summary>
+        public EntityNotFound(string message) : base(message) => Do.Nothing();
+
+        /// <summary>Initializes a new instance of the <see cref="EntityNotFound"/> class.</summary>
+        public EntityNotFound(string message, Exception innerException) : base(message, innerException) => Do.Nothing();
+
+        /// <summary>Initializes a new instance of the <see cref="EntityNotFound"/> class.</summary>
+        protected EntityNotFound(SerializationInfo info, StreamingContext context) : base(info, context) => Do.Nothing();
+
+        /// <inheritdoc />
+        public ValidationSeverity Severity => ValidationSeverity.Error;
+
+        /// <inheritdoc />
+        public string PropertyName => null;
+
+        /// <summary>Creates an <see cref="EntityNotFound"/> for specific ID.</summary>
+        public static EntityNotFound ForId(object id)
+            => new EntityNotFound(string.Format(ValidationMessages.EntityNotFound_ForId, id));
+    }
+}
