cf116ea46e22971a4c9d5bdd3dfc4d7572307c8c
Corniel Nobel <mail@corniel.nl>
2021-08-20 11:38:29 +0200

    Move ToString() logic to create Message, and improve on it.

--- src/Qowaiv.Validation.Abstractions/InvalidModelException.cs
+++ src/Qowaiv.Validation.Abstractions/InvalidModelException.cs
@@ -25,7 +25,7 @@ namespace Qowaiv.Validation.Abstractions
         /// <summary>Initializes a new instance of the <see cref="InvalidModelException"/> class.</summary>
         public InvalidModelException(string message, Exception innerException, IEnumerable<IValidationMessage> messages)
             : this(message, innerException)
-            => Errors = new ReadOnlyCollection<IValidationMessage>(messages.Where(e => e.Severity >= ValidationSeverity.Error).ToArray());
+            => Errors = new ReadOnlyCollection<IValidationMessage>(Filter(messages).ToArray());
 
         /// <summary>Initializes a new instance of the <see cref="InvalidModelException"/> class.</summary>
         protected InvalidModelException(SerializationInfo info, StreamingContext context)
@@ -52,22 +52,38 @@ namespace Qowaiv.Validation.Abstractions
         public static InvalidModelException For<T>(IEnumerable<IValidationMessage> messages)
         {
             var sb = new StringBuilder().AppendFormat(QowaivValidationMessages.InvalidModelException, typeof(T));
-            
-            foreach(var message in messages ?? Array.Empty<IValidationMessage>())
+            var filtered = Filter(messages);
+
+            if (filtered.Any())
             {
-                if (!string.IsNullOrEmpty(message.Message))
+                sb.Remove(sb.Length - 1, 1).AppendLine(":");
+                foreach (var message in filtered)
                 {
-                    sb.Append("* ");
-                    var lines = message.Message.Split(NewLine, StringSplitOptions.None);
-                    sb.AppendLine(lines[0]);
-                    foreach(var line in lines.Skip(1))
-                    {
-                        sb.Append("  ").AppendLine(line);
-                    }
+                    Append(sb, message);
                 }
             }
             return new InvalidModelException(sb.ToString(), null, messages);
         }
+
+        private static IEnumerable<IValidationMessage> Filter(IEnumerable<IValidationMessage> messages)
+            => (messages ?? Array.Empty<IValidationMessage>())
+            .Where(e => e.Severity >= ValidationSeverity.Error);
+
+        private static void Append(StringBuilder builder, IValidationMessage message)
+        {
+            var text = string.IsNullOrWhiteSpace(message.Message)
+                ? "Validation Error."
+                : message.Message.Trim();
+            var lines = text.Split(NewLine, StringSplitOptions.None);
+            builder
+                .Append("* ")
+                .Append(string.Join(Environment.NewLine + "  ", lines))
+                .AppendLine(PropertySuffix(message));
+        }
+
+        private static string PropertySuffix(IValidationMessage message)
+            => string.IsNullOrWhiteSpace(message.PropertyName) ? string.Empty : $" ({message.PropertyName})";
+
         private static readonly string[] NewLine = new[] { "\r\n", "\n" };
     }
 }
