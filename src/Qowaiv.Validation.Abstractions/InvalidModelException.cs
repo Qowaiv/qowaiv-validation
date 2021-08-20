d460240db4882b65d5511af209614588c9ae2454
Corniel Nobel <mail@corniel.nl>
2021-08-20 13:23:34 +0200

    Improve on Invalid model exception message (#31)

--- src/Qowaiv.Validation.Abstractions/InvalidModelException.cs
+++ src/Qowaiv.Validation.Abstractions/InvalidModelException.cs
@@ -25,7 +25,7 @@ namespace Qowaiv.Validation.Abstractions
         /// <summary>Initializes a new instance of the <see cref="InvalidModelException"/> class.</summary>
         public InvalidModelException(string message, Exception innerException, IEnumerable<IValidationMessage> messages)
             : this(message, innerException)
-            => Errors = new ReadOnlyCollection<IValidationMessage>(messages.Where(e => e.Severity >= ValidationSeverity.Error).ToArray());
+            => Errors = new ReadOnlyCollection<IValidationMessage>(Filter(messages).ToArray());
 
         /// <summary>Initializes a new instance of the <see cref="InvalidModelException"/> class.</summary>
         protected InvalidModelException(SerializationInfo info, StreamingContext context)
@@ -45,32 +45,45 @@ namespace Qowaiv.Validation.Abstractions
             info.AddValue(nameof(Errors), Errors.ToArray());
         }
 
-        /// <inheritdoc />
-        public override string ToString()
-        {
-            var sb = new StringBuilder().AppendLine(base.ToString());
-            foreach (var error in Errors)
-            {
-                sb.Append(error.Message);
-                if (!string.IsNullOrEmpty(error.PropertyName))
-                {
-                    sb.Append(" (").Append(error.PropertyName).AppendLine(")");
-                }
-                else
-                {
-                    sb.AppendLine();
-                }
-            }
-            return sb.ToString();
-        }
-
         /// <summary>The related validation error(s).</summary>
         public IReadOnlyList<IValidationMessage> Errors { get; } = new ReadOnlyCollection<IValidationMessage>(new IValidationMessage[0]);
 
         /// <summary>Creates an <see cref="InvalidModelException"/> for the model.</summary>
         public static InvalidModelException For<T>(IEnumerable<IValidationMessage> messages)
         {
-            return new InvalidModelException(string.Format(QowaivValidationMessages.InvalidModelException, typeof(T)), null, messages);
+            var sb = new StringBuilder().AppendFormat(QowaivValidationMessages.InvalidModelException, typeof(T));
+            var filtered = Filter(messages);
+
+            if (filtered.Any())
+            {
+                sb.Remove(sb.Length - 1, 1).AppendLine(":");
+                foreach (var message in filtered)
+                {
+                    Append(sb, message);
+                }
+            }
+            return new InvalidModelException(sb.ToString(), null, messages);
         }
+
+        private static IEnumerable<IValidationMessage> Filter(IEnumerable<IValidationMessage> messages)
+            => (messages ?? Array.Empty<IValidationMessage>())
+            .Where(e => e.Severity >= ValidationSeverity.Error);
+
+        private static void Append(StringBuilder builder, IValidationMessage message)
+        {
+            var text = string.IsNullOrWhiteSpace(message.Message)
+                ? "Validation Error."
+                : message.Message.Trim();
+            var lines = text.Split(NewLine, StringSplitOptions.None);
+            builder
+                .Append("* ")
+                .Append(string.Join(Environment.NewLine + "  ", lines))
+                .AppendLine(PropertySuffix(message));
+        }
+
+        private static string PropertySuffix(IValidationMessage message)
+            => string.IsNullOrWhiteSpace(message.PropertyName) ? string.Empty : $" ({message.PropertyName})";
+
+        private static readonly string[] NewLine = new[] { "\r\n", "\n" };
     }
 }
