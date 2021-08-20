94adff55b92fbf5ef604285196d0542467f074ee
Corniel Nobel <mail@corniel.nl>
2021-08-20 11:48:40 +0200

    Specs.

--- test/Qowaiv.Validation.Abstractions.UnitTests/Exception_specs.cs
+++ test/Qowaiv.Validation.Abstractions.UnitTests/Exception_specs.cs
@@ -8,21 +8,46 @@ namespace Exception_specs
     public class InvalidModel
     {
         [Test]
-        public void Contains_the_error_messages_in_the_ToString()
+        public void Null_messages_is_fine()
         {
-            var str = InvalidModelException.For<int>(new IValidationMessage[]
+            var exception = InvalidModelException.For<int>(null);
+            Assert.That(exception.Message, Is.EqualTo("The System.Int32 model can not be operated on as it is invalid."));
+        }
+
+        [Test]
+        public void No_list_without_errors_specified()
+        {
+            var exception = InvalidModelException.For<int>(new IValidationMessage[]
+            {
+                ValidationMessage.None,
+            });
+            Assert.That(exception.Message, Is.EqualTo("The System.Int32 model can not be operated on as it is invalid."));
+        }
+        
+        [Test]
+        public void Contains_the_error_messages_in_Message()
+        {
+            var exception = InvalidModelException.For<int>(new IValidationMessage[]
             {
                 ValidationMessage.None,
                 ValidationMessage.Error("Not a prime", "_value"),
+                ValidationMessage.Error("Multi-line\nmessage.", "multi-line"),
+                ValidationMessage.Error("No property specified."),
+                ValidationMessage.Error("  ", "no-message"),
                 ValidationMessage.Warn("Small", "_value"),
                 ValidationMessage.Info("Not my favorite", "_value"),
                 ValidationMessage.Error("Not a multiple of 17", "_value"),
-            }).ToString();
+            });
 
-            Assert.AreEqual(@"Qowaiv.Validation.Abstractions.InvalidModelException: The System.Int32 model can not be operated on as it is invalid.
-Not a prime (_value)
-Not a multiple of 17 (_value)
-", str);
+            Assert.That(exception.Message, Is.EqualTo(
+                @"The System.Int32 model can not be operated on as it is invalid:
+* Not a prime (_value)
+* Multi-line
+  message. (multi-line)
+* No property specified.
+* Validation Error. (no-message)
+* Not a multiple of 17 (_value)
+"));
         }
 
         [Test]
