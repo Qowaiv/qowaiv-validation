1fa3f26409486d5fb546f8772a096d09a550260b
Corniel Nobel <mail@corniel.nl>
2021-09-23 10:33:26 +0200

    update tests.

--- test/Qowaiv.Validation.Guarding.Specs/Must_specs.cs
+++ test/Qowaiv.Validation.Guarding.Specs/Must_specs.cs
@@ -16,14 +16,15 @@ namespace Must_specs
         }
     }
 
-    public class Guards_when
+    public class Continues_when
     {
         [Test]
         public void Must_Be_condition_is_met()
         {
             var model = new TestModel();
-            model.Must().Be(condition: true, ValidationMessage.Error("This is wrong"))
-                .Should().BeValid().WithoutMessages()
+            var result = model.Must().Be(condition: true, ValidationMessage.Error("This is wrong"));
+
+            result.Should().BeValid().WithoutMessages()
                 .Value.Should().BeSameAs(model);
         }
 
@@ -31,8 +32,9 @@ namespace Must_specs
         public void Must_NotBe_condition_is_not_met()
         {
             var model = new TestModel();
-            model.Must().NotBe(condition: false, "This is wrong")
-                .Should().BeValid().WithoutMessages()
+            var result = model.Must().NotBe(condition: false, "This is wrong");
+
+            result.Should().BeValid().WithoutMessages()
                 .Value.Should().BeSameAs(model);
         }
 
@@ -40,35 +42,45 @@ namespace Must_specs
         public void Must_Exist_resolves_entity()
         {
             var model = new TestModel();
-            model.Must().Exist(8, (m, id) => new object())
-                .Should().BeValid().WithoutMessages()
+            var result = model.Must().Exist(8, (m, id) => new object());
+
+            result.Should().BeValid().WithoutMessages()
                 .Value.Should().BeSameAs(model);
         }
     }
 
-    public class Yields_when
+    public class Blocks_when
     {
         [Test]
         public void Must_Be_condition_is_not_met()
-            => new TestModel().Must().Be(condition: false, ValidationMessage.Error("This is wrong"))
-            .Should().BeInvalid().WithMessage(ValidationMessage.Error("This is wrong"));
+        {
+            var result = new TestModel().Must().Be(condition: false, ValidationMessage.Error("This is wrong"));
+            result.Should().BeInvalid().WithMessage(ValidationMessage.Error("This is wrong"));
+        }
 
         [Test]
         public void Must_NotBe_condition_is_met()
-            => new TestModel().Must().NotBe(condition: true, "This is wrong")
-            .Should().BeInvalid().WithMessage(ValidationMessage.Error("This is wrong"));
+        { 
+            var result = new TestModel().Must().NotBe(condition: true, "This is wrong");
+            result.Should().BeInvalid().WithMessage(ValidationMessage.Error("This is wrong"));
+        }
 
         [Test]
         public void Must_Exist_does_not_resolve_entity()
-          => new TestModel().Must().Exist(666, (m, id) => (object)null)
-            .Should().BeInvalid().WithMessage(ValidationMessage.Error("Entity with ID 666 could not be found."));
+        {
+            var result = new TestModel().Must().Exist(666, (m, id) => (object)null);
+            result.Should().BeInvalid().WithMessage(ValidationMessage.Error("Entity with ID 666 could not be found."));
+        }
     }
 
     public class ToString_as_type
     {
         [Test]
         public void Displays_type_based_on_TSubject()
-            => new TestModel().Must().ToString().Should().Be("Must<Must_specs.TestModel>");
+        {
+            var result = new TestModel().Must();
+            result.ToString().Should().Be("Must<Must_specs.TestModel>");
+        }
     }
 
     internal class TestModel { }
