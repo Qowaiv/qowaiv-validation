7cdd33a0297175184b940d09c75c4e95b52e0fd5
Corniel Nobel <mail@corniel.nl>
2022-06-30 11:49:20 +0200

    Added test for Nested Model attribute.

---
+++ specs/Qowaiv.Validation.Specs/DataAnnotations/Attributes/Nested_model_specs.cs
@@ -0,0 +1,40 @@
+﻿namespace Data_annotations.Attributes.Nested_model_specs;
+
+public class Child_model
+{
+    [Test]
+    public void decorated_with_nested_model_attribute_valides_grand_children()
+        => new WithNestedChild().Should()
+        .BeInvalidFor(new AnnotatedModelValidator<WithNestedChild>());
+
+    [Test]
+    public void not_decorated_with_nested_model_attribute_skips_grand_children()
+        => new WithoutNestedChild().Should()
+        .BeValidFor(new AnnotatedModelValidator<WithNestedChild>());
+}
+
+internal class WithNestedChild
+{
+    public WithNestedChild Child { get; } = new();
+}
+
+internal class WithoutNestedChild
+{
+    public NotNestedChild Child { get; } = new();
+}
+
+[NestedModel]
+internal class NestedChild
+{
+    public GrandChild Child { get; } = new();
+}
+internal class NotNestedChild
+{
+    public GrandChild Child { get; } = new();
+}
+
+internal class GrandChild
+{
+    [Mandatory]
+    public int? Answer { get; set; }
+}
