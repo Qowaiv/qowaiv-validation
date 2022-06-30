cc8ff1e452e3d961c6d8bd6054af5c1641543ed8
Corniel Nobel <mail@corniel.nl>
2022-06-30 11:08:12 +0200

    Remove old tests.

--- test/Qowaiv.Validation.DataAnnotations.UnitTests/DataAnnotationsValidator_specs.cs
+++ test/Qowaiv.Validation.DataAnnotations.UnitTests/DataAnnotationsValidator_specs.cs
@@ -5,59 +5,6 @@ namespace DataAnnotationsValidator_specs;
 public class Validates
 {
     [Test]
-    public void ModelWithMandatoryProperties_with_errors()
-    {
-        using (CultureInfoScope.NewInvariant())
-        {
-            new ModelWithMandatoryProperties()
-                .Should().BeInvalidFor(new AnnotatedModelValidator<ModelWithMandatoryProperties>())
-                .WithMessages(
-                    ValidationMessage.Error("The E-mail address field is required.", "Email"),
-                    ValidationMessage.Error("The SomeString field is required.", "SomeString"));
-        }
-    }
-
-    [Test]
-    public void ModelWithMandatoryProperties_as_valid()
-        => new ModelWithMandatoryProperties
-        {
-            Email = EmailAddress.Parse("info@qowaiv.org"),
-            SomeString = "Some value",
-        }
-        .Should().BeValidFor(new AnnotatedModelValidator<ModelWithMandatoryProperties>());
-
-    [Test]
-    public void ModelWithAllowedValues_with_error()
-        => new ModelWithAllowedValues
-        {
-            Country = Country.TR
-        }
-        .Should().BeInvalidFor(new AnnotatedModelValidator<ModelWithAllowedValues>())
-        .WithMessage(ValidationMessage.Error("The value of the Country field is not allowed.", "Country"));
-
-    [Test]
-    public void Validate_ModelWithAllowedValues_is_valid()
-        => new ModelWithAllowedValues()
-        .Should().BeValidFor(new AnnotatedModelValidator<ModelWithAllowedValues>());
-
-    [Test]
-    public void ModelWithForbiddenValues_with_error()
-        => new ModelWithForbiddenValues
-        {
-            Email = EmailAddress.Parse("spam@qowaiv.org"),
-        }
-        .Should().BeInvalidFor(new AnnotatedModelValidator<ModelWithForbiddenValues>())
-        .WithMessage(ValidationMessage.Error("The value of the Email field is not allowed.", "Email"));
-
-    [Test]
-    public void ModelWithForbiddenValues_as_valid()
-        => new ModelWithForbiddenValues
-        {
-            Email = EmailAddress.Parse("info@qowaiv.org"),
-        }
-        .Should().BeValidFor(new AnnotatedModelValidator<ModelWithForbiddenValues>());
-
-    [Test]
     public void NestedModelWithInvalidChildren_with_error()
         => new NestedModelWithChildren
         {
@@ -99,46 +46,6 @@ public class Validates
         => new ModelThatReturnsNoneMessage()
         .Should().BeValidFor(new AnnotatedModelValidator<ModelThatReturnsNoneMessage>())
         .WithoutMessages();
-}
-
-public class Validates_
-{
-    [Test]
-    public void Model_with_valid_IValidatableObject_child()
-    {
-        var model = new ModelWithIValidatableObjectChild()
-        {
-            Child = new(){ Answer = 42 },
-        };
-        model.Should().BeValidFor(new AnnotatedModelValidator<ModelWithIValidatableObjectChild>())
-        .WithoutMessages();
-    }
-
-    [Test]
-    public void Model_with_valid_decorated_child()
-    {
-        var model = new ModelWithDecoratedChild()
-        {
-            Child = new() { Answer = 42 },
-        };
-        model.Should().BeValidFor(new AnnotatedModelValidator<ModelWithDecoratedChild>())
-        .WithoutMessages();
-    }
-}
-
-public class Invalidates
-{
-    [Test]
-    public void Model_with_invalid_IValidatableObject_child()
-        => new ModelWithIValidatableObjectChild()
-        .Should().BeInvalidFor(new AnnotatedModelValidator<ModelWithIValidatableObjectChild>())
-        .WithMessage(ValidationMessage.Error("Answer to the Ultimate Question of Life, the Universe, and Everything.", "Child.Answer"));
-
-    [Test]
-    public void Model_with_invalid_decorated_child()
-        => new ModelWithDecoratedChild()
-        .Should().BeInvalidFor(new AnnotatedModelValidator<ModelWithDecoratedChild>())
-        .WithMessage(ValidationMessage.Error("Answer to the Ultimate Question of Life, the Universe, and Everything.", "Child"));
 }
 
 public class Validates_without_crashing
