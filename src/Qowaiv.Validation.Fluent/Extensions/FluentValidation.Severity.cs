5756f9da67d17c8138796e32848334938eb24d47
Corniel Nobel <corniel@gmail.com>
2023-03-07 09:36:48 +0100

    Apply suggestions. (#63)

--- src/Qowaiv.Validation.Fluent/Extensions/FluentValidation.Severity.cs
+++ src/Qowaiv.Validation.Fluent/Extensions/FluentValidation.Severity.cs
@@ -1,17 +1,16 @@
-﻿namespace FluentValidation
+﻿namespace FluentValidation;
+
+/// <summary>Extensions on <see cref="Severity"/>.</summary>
+public static class QowaivSeverityExtensions
 {
-    /// <summary>Extensions on <see cref="Severity"/>.</summary>
-    public static class QowaivSeverityExtensions
-    {
-        /// <summary>Converts <see cref="Severity"/> to <see cref="ValidationSeverity"/>.</summary>
-        [Pure]
-        public static ValidationSeverity ToValidationSeverity(this Severity severity)
-            => Guard.DefinedEnum(severity, nameof(severity)) switch
-            {
-                Severity.Error => ValidationSeverity.Error,
-                Severity.Warning => ValidationSeverity.Warning,
-                Severity.Info => ValidationSeverity.Info,
-                _ => ValidationSeverity.None,
-            };
-    }
+    /// <summary>Converts <see cref="Severity"/> to <see cref="ValidationSeverity"/>.</summary>
+    [Pure]
+    public static ValidationSeverity ToValidationSeverity(this Severity severity)
+        => Guard.DefinedEnum(severity, nameof(severity)) switch
+        {
+            Severity.Error => ValidationSeverity.Error,
+            Severity.Warning => ValidationSeverity.Warning,
+            Severity.Info => ValidationSeverity.Info,
+            _ => ValidationSeverity.None,
+        };
 }
--- src/Qowaiv.Validation.Messages/EntityNotFound.cs
+++ src/Qowaiv.Validation.Messages/EntityNotFound.cs
@@ -1,35 +1,29 @@
-﻿using Qowaiv.Validation.Abstractions;
-using System;
-using System.Diagnostics.Contracts;
-using System.Runtime.Serialization;
+﻿namespace Qowaiv.Validation.Messages;
 
-namespace Qowaiv.Validation.Messages
+/// <summary>Message to communicate if a entity could not be found.</summary>
+[Serializable]
+public class EntityNotFound : InvalidOperationException, IValidationMessage
 {
-    /// <summary>Message to communicate if a entity could not be found.</summary>
-    [Serializable]
-    public class EntityNotFound : InvalidOperationException, IValidationMessage
-    {
-        /// <summary>Initializes a new instance of the <see cref="EntityNotFound"/> class.</summary>
-        public EntityNotFound() : this(ValidationMessages.EntityNotFound) => Do.Nothing();
+    /// <summary>Initializes a new instance of the <see cref="EntityNotFound"/> class.</summary>
+    public EntityNotFound() : this(ValidationMessages.EntityNotFound) => Do.Nothing();
 
-        /// <summary>Initializes a new instance of the <see cref="EntityNotFound"/> class.</summary>
-        public EntityNotFound(string message) : base(message) => Do.Nothing();
+    /// <summary>Initializes a new instance of the <see cref="EntityNotFound"/> class.</summary>
+    public EntityNotFound(string message) : base(message) => Do.Nothing();
 
-        /// <summary>Initializes a new instance of the <see cref="EntityNotFound"/> class.</summary>
-        public EntityNotFound(string message, Exception innerException) : base(message, innerException) => Do.Nothing();
+    /// <summary>Initializes a new instance of the <see cref="EntityNotFound"/> class.</summary>
+    public EntityNotFound(string message, Exception innerException) : base(message, innerException) => Do.Nothing();
 
-        /// <summary>Initializes a new instance of the <see cref="EntityNotFound"/> class.</summary>
-        protected EntityNotFound(SerializationInfo info, StreamingContext context) : base(info, context) => Do.Nothing();
+    /// <summary>Initializes a new instance of the <see cref="EntityNotFound"/> class.</summary>
+    protected EntityNotFound(SerializationInfo info, StreamingContext context) : base(info, context) => Do.Nothing();
 
-        /// <inheritdoc />
-        public ValidationSeverity Severity => ValidationSeverity.Error;
+    /// <inheritdoc />
+    public ValidationSeverity Severity => ValidationSeverity.Error;
 
-        /// <inheritdoc />
-        public string? PropertyName => null;
+    /// <inheritdoc />
+    public string? PropertyName => null;
 
-        /// <summary>Creates an <see cref="EntityNotFound"/> for specific ID.</summary>
-        [Pure]
-        public static EntityNotFound ForId(object id)
-            => new(string.Format(ValidationMessages.EntityNotFound_ForId, id));
-    }
+    /// <summary>Creates an <see cref="EntityNotFound"/> for specific ID.</summary>
+    [Pure]
+    public static EntityNotFound ForId(object id)
+        => new(string.Format(ValidationMessages.EntityNotFound_ForId, id));
 }
