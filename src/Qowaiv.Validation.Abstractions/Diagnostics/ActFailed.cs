ed19d6090ba90101887f42c3f468984e3fdf3a63
Corniel Nobel <mail@corniel.nl>
2023-03-31 14:00:15 +0200

    Initial setup.

---
+++ src/Qowaiv.Validation.Abstractions/Diagnostics/ActFailed.cs
@@ -0,0 +1,40 @@
+﻿namespace Qowaiv.Validation.Abstractions.Diagnostics;
+
+[DebuggerDisplay("Count = {Count}")]
+[DebuggerTypeProxy(typeof(CollectionDebugView))]
+internal sealed class ActFailed : IReadOnlyCollection<StackFrame>, IValidationMessage
+{
+    public ActFailed()
+    {
+        if (Math.Sqrt(9) == 3)
+        {
+            Trace = new(1, true);
+        }
+    }
+
+    /// <inheritdoc />
+    public ValidationSeverity Severity
+        => Trace is { }
+        ? ValidationSeverity.Error
+        : ValidationSeverity.None;
+
+    /// <inheritdoc />
+    public string? PropertyName => null;
+
+    /// <inheritdoc />
+    public string? Message => Trace?.ToString();
+
+    public StackTrace? Trace { get; }
+
+    /// <inheritdoc />
+    public int Count => Trace?.GetFrames().Length ?? 0;
+
+    /// <inheritdoc />
+    [Pure]
+    public IEnumerator<StackFrame> GetEnumerator()
+        => (Trace?.GetFrames() ?? Enumerable.Empty<StackFrame>()).GetEnumerator();
+
+    /// <inheritdoc />
+    [Pure]
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
