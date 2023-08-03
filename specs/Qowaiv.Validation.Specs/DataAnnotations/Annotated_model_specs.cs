49c090da9638fc3684282bdef38f20eda2caaee2
Corniel Nobel <corniel@gmail.com>
2023-08-03 15:58:30 +0200

    Marked [NestedModel] attribute as obsolete. (#70)

--- specs/Qowaiv.Validation.Specs/DataAnnotations/Annotated_model_specs.cs
+++ specs/Qowaiv.Validation.Specs/DataAnnotations/Annotated_model_specs.cs
@@ -62,21 +62,3 @@ public class Has_no_properties_for
     public void enums()
         => AnnotatedModel.Get(typeof(TypeCode)).Properties.Should().BeEmpty();
 }
-
-public  class With_debugger_experience
-{
-    [Test]
-    public void Has_debugger_display()
-    {
-        var annotated = AnnotatedModel.Get(typeof(Model));
-        annotated.Should().HaveDebuggerDisplay("Data_annotations.Annotated_model_specs.With_debugger_experience+Model, Attributes: 1, Properties: 2");
-    }
-
-    [NestedModel]
-    internal class Model
-    {
-        public Guid Id { get; }
-        
-        public string? Name { get; }
-    }
-}
