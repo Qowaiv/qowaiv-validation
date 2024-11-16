0845f97b5dff4c9f7a5c0ab86c83bbbd77b059d7
Corniel Nobel <corniel@gmail.com>
2024-11-16 11:26:15 +0100

    Use (internal) Qowaiv.Diagnostics.Contracts.

--- src/Qowaiv.Validation.Abstractions/Diagnostics/Contracts/ImpureAttribute.cs
+++
@@ -1,6 +0,0 @@
-namespace Qowaiv.Validation.Abstractions.Diagnostics.Contracts;
-
-/// <summary>To mark a method explicitly as impure.</summary>
-[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
-[Conditional("CONTRACTS_FULL")]
-public class ImpureAttribute : Attribute { }
