5051481c001deed9746896eb04f4798fdbe115dc
Corniel Nobel <corniel@gmail.com>
2025-03-25 21:52:38 +0100

    Simplify

--- src/Qowaiv.Validation.DataAnnotations/MiniValidation2/PropertyDetails.cs
+++ src/Qowaiv.Validation.DataAnnotations/MiniValidation2/PropertyDetails.cs
@@ -1,9 +1,7 @@
 namespace MiniValidation2;
 
-internal record PropertyDetails(string Name, DisplayAttribute? DisplayAttribute, Type Type, Func<object, object?> PropertyGetter, ValidationAttribute[] ValidationAttributes, bool Recurse, Type? EnumerableType)
+internal record PropertyDetails(string Name, DisplayAttribute? DisplayAttribute, Type Type, Func<object, object?> GetValue, ValidationAttribute[] ValidationAttributes, bool Recurse, Type? EnumerableType)
 {
-    public object? GetValue(object target) => PropertyGetter(target);
-
     public bool IsEnumerable => EnumerableType != null;
 
     public bool HasValidationAttributes => ValidationAttributes.Length > 0;
