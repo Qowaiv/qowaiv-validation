7b65d44e172f82cd1907373a278d73a4d542a94e
Corniel Nobel <corniel@gmail.com>
2025-03-25 22:07:34 +0100

    seal

--- src/Qowaiv.Validation.DataAnnotations/MiniValidation2/PropertyDetails.cs
+++ src/Qowaiv.Validation.DataAnnotations/MiniValidation2/PropertyDetails.cs
@@ -1,6 +1,6 @@
 namespace MiniValidation2;
 
-internal record PropertyDetails(string Name, DisplayAttribute? DisplayAttribute, Type Type, Func<object, object?> GetValue, ValidationAttribute[] ValidationAttributes, bool Recurse, Type? EnumerableType)
+internal sealed record PropertyDetails(string Name, DisplayAttribute? DisplayAttribute, Type Type, Func<object, object?> GetValue, ValidationAttribute[] ValidationAttributes, bool Recurse, Type? EnumerableType)
 {
     public bool HasValidationAttributes => ValidationAttributes.Length > 0;
 }
