cf4175f8f17c169d1703730f02ef689e14e9fe8e
Corniel Nobel <corniel@gmail.com>
2026-05-20 18:49:33 +0200

    Format

--- specs/Qowaiv.Validation.Specs/Fluent/Models/PostalCodeModel.cs
+++ specs/Qowaiv.Validation.Specs/Fluent/Models/PostalCodeModel.cs
@@ -10,5 +10,6 @@ public class PostalCodeModel
 
 public class PostalCodeModelValidator : AbstractValidator<PostalCodeModel>
 {
-    public PostalCodeModelValidator() => RuleFor(m => m.PostalCode).ValidFor(m => m.Country);
+    public PostalCodeModelValidator()
+        => RuleFor(m => m.PostalCode).ValidFor(m => m.Country);
 }
